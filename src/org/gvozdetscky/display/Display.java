1e87a7a7d9b74dd5a47f1435f4fe07fdb96552a0
Egor <gvozdetscky.egor@yandex.ru>
2017-04-15 00:43:25 +0400

    Добавлена двойная буфферизация

--- src/org/gvozdetscky/display/Display.java
+++ src/org/gvozdetscky/display/Display.java
@@ -2,6 +2,10 @@ package org.gvozdetscky.display;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.Arrays;
 
 /**
  * Created by Egor on 14.04.2017.
@@ -12,35 +16,61 @@ public abstract class Display {
     private static JFrame window;
     private static Canvas content;
 
-    public static void create(int width, int height,  String title) {
+    private static BufferedImage buffer;
+    private static int[] bufferData;
+    private static Graphics bufferGraphics;
+    private static int clearColor;
+
+    private static BufferStrategy bufferStrategy;
+
+    private static float delta = 0;
+
+    public static void create(int width, int height,  String title, int _clearColor, int numBuffers) {
         if (created)
             return;
         window = new JFrame(title);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        content = new Canvas() {
-            public void paint(Graphics graphics) {
-                super.paint(graphics);
-                render(graphics);
-            }
-        };
+        content = new Canvas();
 
         Dimension size = new Dimension(width, height);
         content.setPreferredSize(size);
-        content.setBackground(Color.BLACK);
 
         window.setResizable(false);
         window.getContentPane().add(content);
         window.pack();
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+
+        buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        bufferData = ((DataBufferInt) buffer.getRaster().getDataBuffer()).getData();
+        bufferGraphics = buffer.getGraphics();
+        clearColor = _clearColor;
+
+        content.createBufferStrategy(numBuffers);
+        bufferStrategy = content.getBufferStrategy();
+
+        created = true;
+    }
+
+    public static void clear() {
+        Arrays.fill(bufferData, clearColor);
+
     }
 
     public static void render() {
-        content.repaint();
+        bufferGraphics.setColor(new Color(0x0000ff));
+        bufferGraphics.fillOval((int) (350 + (Math.sin(delta) * 200)), 250, 100, 100);
+
+        ((Graphics2D) bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        bufferGraphics.fillOval((int) (500   + (Math.sin(delta) * 200)), 250, 100, 100);
+        ((Graphics2D) bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+
+        //delta += 0.02f;
     }
 
-    private static void render(Graphics graphics) {
-        graphics.setColor(Color.WHITE);
-        graphics.fillOval(400 - 50, 300 - 50, 100, 100);
+    public static void swapBuffer() {
+        Graphics graphics = bufferStrategy.getDrawGraphics();
+        graphics.drawImage(buffer, 0, 0, null);
+        bufferStrategy.show();
     }
 }
--- src/org/gvozdetscky/main/Main.java
+++ src/org/gvozdetscky/main/Main.java
@@ -11,12 +11,16 @@ import java.awt.event.ActionEvent;
  */
 public class Main {
     public static void main(String[] args) {
-        Display.create(800, 600, "Tanks");
+        Display.create(800, 600, "Tanks", 0xFF00FF00, 3);
 
         Timer timer = new Timer(1000 / 60, new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
+                Display.clear();
+
                 Display.render();
+
+                Display.swapBuffer();
             }
         });
 
